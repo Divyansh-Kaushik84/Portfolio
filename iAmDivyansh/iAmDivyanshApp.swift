f4a9ec61aaffb1107f413e92f0c2e9ee2f6fdb0b
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-08-22 02:27:21 +0530

    Initial Commit

---
+++ iAmDivyansh/iAmDivyanshApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAmDivyanshApp.swift
+//  iAmDivyansh
+//
+//  Created by Divyansh Kaushik on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct iAmDivyanshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
